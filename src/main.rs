a4a37cfa3ee01786ea9ab4dc649ac0fffcc028b7
Kamil Democko <kamil.democko@gmail.com>
2025-02-16 18:17:50 +0100

    added printing - row del

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ const TICK: Duration = Duration::from_millis(8);
 
 fn main() {
     let now = Instant::now();
+
+    println!("start");
     loop {
         let since = now.elapsed();
-        // print!("\x08\r{:?}", since);
-        print!("\r{:?}", since);
+        println!("{:?}", since);
         _ = io::stdout().flush();
 
         thread::sleep(TICK);
+
+        print!("\x1b[1A\x1b[2K"); // move up 1 line and clear it and move down
     }
 }
